1679255481a8a54e85e91fb7091f476ed27e5f1b
Attikittyalt1 <103009142+Attikittyalt1@users.noreply.github.com>
2026-08-28 14:34:08 -0500

    fixed mutation bundle. still untested

--- src/mutations/MutationBundle.cs
+++ src/mutations/MutationBundle.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace NinoChess.Mutations;
 
-public class MutationBundle(FullBoardState currentBoardState, object? sender, IEnumerable<BoardStateEvent> mutations) : BoardStateEvent(currentBoardState, sender)
+public class MutationBundle(FullBoardState currentBoardState, object? sender, IEnumerable<IBoardStateMutation> mutations) : BoardStateEvent(currentBoardState, sender)
 {
     public override void Execute()
     {
@@ -14,5 +14,5 @@ public class MutationBundle(FullBoardState currentBoardState, object? sender, IE
     }
 
 
-    public override IBoardStateMutation GetInverse() => new MutationBundle(currentBoardState, sender, Enumerable.Reverse(mutations));
+    public override IBoardStateMutation GetInverse() => new MutationBundle(currentBoardState, sender, Enumerable.Reverse(mutations.Select(mutation => mutation.GetInverse())));
 }
